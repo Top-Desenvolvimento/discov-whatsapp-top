51ba158940e5abc1dc42b68ebafc565dab43c79c
Top-Desenvolvimento <topesteticabucal.gerencia2@gmail.com>
2026-06-29 09:24:05 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,32 @@
+export const cidades = [
+  "Caxias do Sul",
+  "Farroupilha",
+  "Flores da Cunha"
+  "Bento Gonçalves",
+  "Encantado",
+  "Soledade",
+  "Veranópolis",
+  
+];
 
+export const campanhas = [
+  "Implantes",
+  "Ortodontia",
+  "Avaliação",
+  "Profilaxia",
+  "Revisão",
+  "Reativação",
+  "Pós-venda",
+  "Personalizada"
+];
+
+export const resultados = [
+  "Agendou",
+  "Interessado",
+  "Não respondeu",
+  "Não atende",
+  "Número inválido",
+  "Sem interesse",
+  "Já é paciente",
+  "Reagendar"
+];
